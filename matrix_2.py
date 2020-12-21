41159d9def09f4d7bef2403c1e8410babb2532a8
Vladimir <rashvs1985@gmail.com>
2020-12-21 10:34:31 +0000

    ДЗ 29. Матрица 2

--- matrix_2.py
+++ matrix_2.py
@@ -2,67 +2,35 @@ from random import randint
 
 lst = []
 tmp = []
-
-def sorter(l, t):
-# Сумма элементов в столбцах
-	for i in range (len(l)):
-		for j in range (len(l)):
-			t[i] += lst[j][i]
-	
-# Сотрировка положения столбцов по возрастанию сумм
-	for y in range (m):
-		for x in range (m - 1 - y):
-			if t[x] > t[x+1]:
-				t[x], t[x+1] = t[x+1], t[x]
-				for z in range (m):
-					l[z][x], l[z][x+1] = l[z][x+1], l[z][x]
-
-# Сортировка элементов в столбцах по условию
-
-# четный растет сверху вниз
-	for x in range (1, m, 2):
-		for y in range (m - 1):
-			for z in range (m - 1):
-				if lst[z][x] > lst[z+1][x]:
-					lst[z][x], lst[z+1][x] = lst[z+1][x], lst[z][x]
-
-# нечетный - снизу вверх
-	for x in range(0, m, 2):
-		for y in range (m - 1):
-			for z in range (m-1):
-				if lst[z][x] < lst[z+1][x]:
-					lst[z][x], lst[z+1][x] = lst[z+1][x], lst[z][x]
-
-
-def output(l, t):
-	for y in range (m):
-		for x in range (m):
-			if l[y][x] < 10:
-				print(l[y][x], '    ', end='')
-			else:
-				print(l[y][x], '   ' , end='')
-		print()
-	print()
-	for z in range (m):
-		if t[z] < 10:
-			print(t[z], ' ', end='')
-		else:
-			print(t[z], '  ', end='')
-	print()
+s = 0
 
 while True:
-	try:
-		m = int(input('Введите число строк: '))
-		n = int(input('Введите число столбцов: '))
-	except ValueError:
-		print('Некорректное значение!')
-	else:
-		if m > 0 or n > 0: break
-		else: print('Некорректная величина!')
+    try:
+        m = int(input('Введите число строк: '))
+        n = int(input('Введите число столбцов: '))
+    except ValueError:
+        print('Некорректное значение!')
+    else:
+        if m > 0 and n > 0: break
+        else: print('Некорректная величина!')
 
-for i in range (m):
-	lst.insert(i, [randint(1, 50) for _ in range (n)])
+for i in range(m):
+    lst.insert(i, [randint(1, 50) for _ in range(n)])
 
-tmp = [0] * m
-sorter(lst, tmp)
-output(lst, tmp)
+tmp = [0] * n
+
+# Считаем и складываем в tmp[] сумму слобцов
+for i in range(n):
+    for j in range(m):
+        tmp[i] += lst[j][i]
+
+for i in range(m):
+    s = 0
+    for j in range(n):
+        s += lst[i][j]
+        print('{:>4}'.format(lst[i][j]), end='')
+    print('{:>8}'.format(s))
+print()
+for z in range(n):
+    print('{:>4}'.format(tmp[z]), end='')
+print()
